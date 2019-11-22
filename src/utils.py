8756667de08dd5899c70cb555b7cc307377bd9e7
Othman Sefraoui <othman.sefraoui@grenoble-inp.org>
2019-11-22 18:21:42 +0100

    new function in utils

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,8 @@
 # -*- coding: utf-8 -*-
+def select_on(df, columns, dict_elements):
+    ''' Filter columns of df by elements in the dict dict_elements where the keys are the columns'''
+    for column in columns:
+        elements = dict_elements[column]
+        df = df[df[column].isin(elements)]
+    return df
+
