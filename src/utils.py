cf3cc18b8c0202789198cddec366dcd6bc79727d
Quentin Soubeyran <quentin.soubeyran@epfl.ch>
2019-11-23 17:32:25 +0100

    added utils for year plot and area filtering

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,63 @@
 # -*- coding: utf-8 -*-
+from collections.abc import Mapping
+import matplotlib.pyplot as plt
+import pandas as pd
+
 def select_on(df, columns, dict_elements):
     ''' Filter columns of df by elements in the dict dict_elements where the keys are the columns'''
     for column in columns:
-        elements = dict_elements[column]
+        elements = dict_elements.get(column, [])
         df = df[df[column].isin(elements)]
     return df
 
+
+def remove_country_group(df, df_area_code=None):
+    """
+    Remove all entries in the DataFrame for group of countries (e.g. Asia)
+
+    Args:
+        df: the DataFrame to filter
+        df_area_code: dataframe from the standards, indicating which countries are in which country group
+            If not provided, defaults to removing Area Code >= 5000 (see note)
+    
+    Returns:
+        A filtered DataFrame
+    
+    Note:
+        It appears all country-group use "Area Code" >= 5000.
+        However, this is not guaranteed so we prefere to use the standars to get country group code
+    """
+    if df_area_code:
+        return df[~df["Area Code"].isin(df_area_code["Country Group Code"])]
+    else:
+        return df[ df["Area Code"] < 5000]
+
+def plot_completeness_histogram(df_or_dfs, ax=None, figsize=None):
+    """
+    Plot the histogram of how many unique area_code provide data for each year. Used
+    to get an idea on how complete the data are
+
+
+    Args:
+        df_or_dfs: the dataframe to plot the completeness histogram of
+            OR a mapping from names to dataframes
+        ax: matplotlib Axis to draw on
+    Returns:
+        None. Plot the afformention histogram
+    """
+    if ax is None:
+        _, ax = plt.subplots(figsize=figsize)
+    if isinstance(df_or_dfs, Mapping):
+        pd.DataFrame(
+            {
+                name: df.groupby("Year")["Area Code"].nunique()
+                for name, df in df_or_dfs.items()
+            }
+        ).plot.bar(colormap="Paired", ax=ax)
+        ax.legend()
+    else:
+        df_or_dfs.groupby("Year")["Area Code"].nunique().plot.bar(ax=ax)
+    ax.set_xlabel("Year")
+    ax.set_ylabel("# of countries with available data")
+    ax.set_title("Evolution of data completeness")
+    return ax
